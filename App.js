4dc2f559f7fbbaca71ab6ce6d713fa3e6f145a35
Beatriz Schwartz <beatriz.sabbatini@gmail.com>
2020-08-31 09:12:12 -0300

    Added react native paper theme

--- App.js
+++ App.js
@@ -1,15 +1,30 @@
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import 'react-native-gesture-handler';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 import { AddThemeModalProvider } from './src/contexts/AddThemeModalContext';
 import AppNavigator from './src/navigation/index';
+import { colors, metrics } from './src/styles';
 
 export default function App() {
+  const theme = {
+    ...DefaultTheme,
+    roundness: 2,
+    colors: {
+      ...DefaultTheme.colors,
+      primary: colors.Purple,
+      background: colors.White,
+    },
+    roundness: metrics.smallMargin,
+  };
+
   return (
-    <AddThemeModalProvider>
-      <NavigationContainer>
-        <AppNavigator />
-      </NavigationContainer>
-    </AddThemeModalProvider>
+    <PaperProvider theme={theme}>
+      <AddThemeModalProvider>
+        <NavigationContainer>
+          <AppNavigator />
+        </NavigationContainer>
+      </AddThemeModalProvider>
+    </PaperProvider>
   );
 }
